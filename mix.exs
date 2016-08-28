89096eb83963aa3675be01cffa11c2dafd7bacb8
Alan Peabody <gapeabody@gmail.com>
2016-08-28 14:09:45 -0400

    Update mix for release.

--- mix.exs
+++ mix.exs
@@ -4,30 +4,20 @@ defmodule JaResource.Mixfile do
   def project do
     [app: :ja_resource,
      version: "0.1.0",
-     elixir: "~> 1.1",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     source_url: "https://github.com/AgilionApps/ja_resource",
      package: package,
      description: description,
      deps: deps]
   end
 
   # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :phoenix]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:ecto, "~> 2.0"},
@@ -54,7 +44,7 @@ defmodule JaResource.Mixfile do
     """
     A behaviour for defining JSON-API spec controllers in Phoenix.
 
-    Lets you focus on your data, not on boilerplate controller code.
+    Lets you focus on your data, not on boilerplate controller code. Like Webmachine for Phoenix.
     """
   end
 end
