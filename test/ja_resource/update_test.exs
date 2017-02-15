590916073a28987d15fc6ebe03656d709a751dae
John Griffin <john@griffin.io>
2017-02-15 16:05:35 -0500

    Add update successful and invalid callbacks
    Allows user to override the response to successful and invalid model
    updates. Existing behaviour from updated and invalid retained.

--- test/ja_resource/update_test.exs
+++ test/ja_resource/update_test.exs
@@ -24,6 +24,17 @@ defmodule JaResource.UpdateTest do
     end
   end
 
+  defmodule CustomResponseController do
+    use Phoenix.Controller
+    use JaResource.Update
+    def repo, do: JaResourceTest.Repo
+    def model, do: JaResourceTest.Post
+    def handle_invalid_update(conn, errors),
+      do: put_status(conn, 401) |> Phoenix.Controller.render(:errors, data: errors)
+    def render_update(conn, model),
+      do: put_status(conn, :created) |> Phoenix.Controller.render(:show, data: model)
+  end
+
   test "default implementation renders 404 if record not found" do
     conn = prep_conn(:put, "/posts/404", ja_attrs(404, %{"title" => "valid"}))
     response = Update.call(DefaultController, conn)
@@ -62,6 +73,20 @@ defmodule JaResource.UpdateTest do
     conn = prep_conn(:put, "/posts/#{post.id}", ja_attrs(post.id, %{"title" => "invalid"}))
     response = Update.call(CustomController, conn)
     assert response.status == 422
+  end
+
+  test "custom implementation renders 401 if invalid" do
+    {:ok, post} = JaResourceTest.Repo.insert(%JaResourceTest.Post{id: 422})
+    conn = prep_conn(:put, "/posts/#{post.id}", ja_attrs(post.id, %{"title" => "invalid"}))
+    response = Update.call(CustomResponseController, conn)
+    assert response.status == 401
+  end
+
+  test "custom implementation renders 201 if valid" do
+    {:ok, post} = JaResourceTest.Repo.insert(%JaResourceTest.Post{id: 200})
+    conn = prep_conn(:put, "/posts/#{post.id}", ja_attrs(post.id, %{"title" => "valid"}))
+    response = Update.call(CustomResponseController, conn)
+    assert response.status == 201
   end
 
   def prep_conn(method, path, params \\ %{}) do
