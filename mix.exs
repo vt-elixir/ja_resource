39838045e4661d9d375e216342ec32d369d3ac37
Eric Froese <ericfroese@Erics-MacBook-Pro.local>
2018-11-09 17:03:22 -0500

    changed version, whoops

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JaResource.Mixfile do
   def project do
     [
       app: :ja_resource,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.2",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
