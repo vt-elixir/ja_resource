c4d7f77b0ccdeabc4886a03bc7529d221a44f24a
Eric Froese <ericfroese@Erics-MacBook-Pro.local>
2018-11-13 09:21:40 -0500

    just increasing ecto version is enough to be compatible with phoenix 1.4

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule JaResource.Mixfile do
     [
       {:ecto, "~> 3.0"},
       {:plug, "~> 1.2"},
-      {:phoenix, "~> 1.4"},
+      {:phoenix, "~> 1.1"},
       {:ja_serializer, "~> 0.9"},
       {:earmark, "~> 1.0.1", only: :dev},
       {:ex_doc, "~> 0.13", only: :dev}
